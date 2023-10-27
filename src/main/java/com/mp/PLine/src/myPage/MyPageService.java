3c983586d82b015879d7494a96e5957e60e2354e
joeun-01 <jjy020121@gmail.com>
2023-10-28 03:56:22 +0900

    [feat]: 헌혈 리워드 반환 API

--- src/main/java/com/mp/PLine/src/myPage/MyPageService.java
+++ src/main/java/com/mp/PLine/src/myPage/MyPageService.java
@@ -3,14 +3,12 @@ package com.mp.PLine.src.myPage;
 import com.mp.PLine.S3Uploader;
 import com.mp.PLine.config.BaseException;
 import com.mp.PLine.config.BaseResponseStatus;
-import com.mp.PLine.src.myPage.dto.res.GetCertificationRes;
+import com.mp.PLine.src.myPage.dto.res.*;
 import com.mp.PLine.src.myPage.entity.Certification;
 import com.mp.PLine.src.feed.repository.FeedRepository;
 import com.mp.PLine.src.myPage.dto.req.PostCertificationReq;
-import com.mp.PLine.src.myPage.dto.res.FeedRes;
 import com.mp.PLine.src.myPage.dto.util.FeedResI;
 import com.mp.PLine.src.member.entity.Member;
-import com.mp.PLine.src.myPage.dto.res.GetMemberRes;
 import com.mp.PLine.src.myPage.dto.req.PatchMemberReq;
 import com.mp.PLine.utils.entity.Status;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +17,10 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -71,6 +73,7 @@ public class MyPageService {
         return "정보가 수정되었습니다.";
     }
 
+    /* Upload certification API */
     @Transactional
     public Long uploadDonation(Long memberId, MultipartFile image, PostCertificationReq request) throws BaseException, IOException {
         Member member = myPageRepository.findByIdAndStatus(memberId, Status.A)
@@ -87,6 +90,7 @@ public class MyPageService {
         return certifications.getId();
     }
 
+    /* Return certification list API */
     public List<GetCertificationRes> getCertificationList(Long memberId) throws BaseException {
         Member member = myPageRepository.findByIdAndStatus(memberId, Status.A)
                 .orElseThrow(() -> new BaseException(BaseResponseStatus.INVALID_USER));
@@ -103,4 +107,39 @@ public class MyPageService {
                 .collect(Collectors.toList());
     }
 
+    /* Return certification reward list API */
+    public GetRewardRes getRewardList(Long memberId) throws BaseException {
+        // verify user existence using userId
+        Member member = myPageRepository.findByIdAndStatus(memberId, Status.A)
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.INVALID_USER));
+
+        List<Certification> certificationList = certificationRepository.findAllByMemberIdAndStatus(memberId, Status.A);
+        long length = certificationList.size();
+
+        List<RewardRes> rewardList = new ArrayList<>();
+        // 여러 방법을 고민해봤는데.... 메달 개수가 적어서 이렇게 하드코딩하는 게 제일 성능이 베스트일 것 같아 이렇게 했습니다... 배열을 만드는 것도 애매하고... 다 애매했도다
+        if(1 <= length) rewardList.add(RewardRes.from("헌혈 입문자",
+                "https://p-line.s3.ap-northeast-2.amazonaws.com/medal/medal1.png", date(certificationList.get(0).getCreatedAt())));
+        if(3 <= length) rewardList.add(RewardRes.from("생명의 씨앗",
+                "https://p-line.s3.ap-northeast-2.amazonaws.com/medal/medal1.png", date(certificationList.get(2).getCreatedAt())));
+        if(6 <= length) rewardList.add(RewardRes.from("헌혈하는 기쁨",
+                "https://p-line.s3.ap-northeast-2.amazonaws.com/medal/medal1.png", date(certificationList.get(5).getCreatedAt())));
+        if(9 <= length) rewardList.add(RewardRes.from("생명의 빛",
+                "https://p-line.s3.ap-northeast-2.amazonaws.com/medal/medal1.png", date(certificationList.get(8).getCreatedAt())));
+        if(12 <= length) rewardList.add(RewardRes.from("우리 모두의 힘",
+                "https://p-line.s3.ap-northeast-2.amazonaws.com/medal/medal1.png", date(certificationList.get(11).getCreatedAt())));
+        if(15 <= length) rewardList.add(RewardRes.from("헌혈 히어로",
+                "https://p-line.s3.ap-northeast-2.amazonaws.com/medal/medal1.png", date(certificationList.get(14).getCreatedAt())));
+        if(18 <= length) rewardList.add(RewardRes.from("헌혈 마스터",
+                "https://p-line.s3.ap-northeast-2.amazonaws.com/medal/medal1.png", date(certificationList.get(17).getCreatedAt())));
+        if(21 <= length) rewardList.add(RewardRes.from("헌혈의 전설",
+                "https://p-line.s3.ap-northeast-2.amazonaws.com/medal/medal1.png", date(certificationList.get(20).getCreatedAt())));
+
+        return GetRewardRes.of(member, length, rewardList);
+    }
+
+    public String date(Timestamp createdAt) {
+        SimpleDateFormat format = new SimpleDateFormat("yy.MM.dd");
+        return format.format(createdAt);
+    }
 }
