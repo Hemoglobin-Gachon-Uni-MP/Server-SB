339e956c3dad6f55f6627f306d04ab1d629530ff
hyunseok <hyun104467@naver.com>
2023-11-18 01:45:03 +0900

    [Fix] 인증글 조회시 id 누락 개선

--- src/main/java/com/mp/PLine/src/report/dto/CertificationResponseDto.java
+++ src/main/java/com/mp/PLine/src/report/dto/CertificationResponseDto.java
@@ -1,12 +1,17 @@
 package com.mp.PLine.src.report.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class CertificationResponseDto {
+    @ApiModelProperty(example = "헌혈증 ID")
+    private Long id;
+    @ApiModelProperty(example = "닉네임")
     private String member;
+    @ApiModelProperty(example = "헌혈증 번호")
     private String certificateNumber;
     private String certificateImg;
     private String date;
